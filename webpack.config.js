64beaf7eb70522ba26a58f747e5a88034e137e10
manish <manis.kasyap@gmail.com>
2018-01-21 22:30:28 +0400

    installing dependencies and dev dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,19 @@ module.exports = {
     app: "src/app.ts",
     vendor: "src/vendor.ts"
   },
-
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "awesome-typescript-loader"
+      },
+      {
+        test: /\.css$/,
+        loaders: "style-loader!css-loader"
+      }
+    ]
+  },
+  plugins: [new webpack.optimize.UglifyJsPlugin()],
   output: {
     filename: "[name].js"
   }
